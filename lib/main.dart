5a205f05e4b7f9db7527c1ee104a972ef4f3754d
Nam <namnh98@gmail.com>
2024-01-26 18:18:13 +0700

    study variable code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -12,8 +12,67 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  num myNumber = 1;
+  int newIntegerNumber = 2;
+  int newMyNumber = 3;
+  double newDoubleNumber = 3.0;
+  String newStringValue = 'Hello Flutter';
+  String newStringValueOther = 'I am studying';
+  String valueTestString = """new Value
+  value value
+  value """;
+  bool isIntegerNumber = false;
+  var customValue = '';
+  dynamic dynamicValue = 3.0123;
+
+  void checkedType() {
+    print(dynamicValue.runtimeType);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$myNumber',
+                style: const TextStyle(fontSize: 30),
+              ),
+              Text(
+                'so nguyen: $newIntegerNumber',
+                style: const TextStyle(fontSize: 30),
+              ),
+              Text(
+                'so thuc: $newDoubleNumber',
+                style: const TextStyle(fontSize: 30),
+              ),
+              Text(
+                '$newStringValue $newStringValueOther',
+                style: const TextStyle(fontSize: 35),
+              ),
+              Text(
+                valueTestString,
+                style: const TextStyle(fontSize: 35),
+              ),
+              Text(
+                'var: $customValue',
+                style: const TextStyle(fontSize: 35),
+              ),
+              Text(
+                'dynamic value: $dynamicValue',
+                style: const TextStyle(fontSize: 35),
+              ),
+              TextButton(onPressed: checkedType, child: const Text('Check type button'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
